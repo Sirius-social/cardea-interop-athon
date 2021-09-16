40855f3e2a7ceb5938db6939bbc22e7cea88a3f3
Pavel <minikspb@gmail.com>
2021-09-17 02:05:14 +0300

    add GOV, HOLDER, LAB agents

--- defs.py
+++ defs.py
@@ -34,14 +34,45 @@ VERIFIER = {
 HOLDER = {
     'SDK': {
         'server_uri': 'https://demo.socialsirius.com',
-        'credentials': b'hMPfx0D1ptQa2fK8UPw7p9/Zf/UUEY9Ppk9oU92VO8IUHnc6oP5ov7f9PQ1NLIO5EHcqghOJvRoV7taA/vCd2+gRlardPTGGN2IQ2eifQIUF635ie4lgPZGiJSjWYcXm',
+        'credentials': b'hMPfx0D1ptQa2fK8UPw7p9/Zf/UUEY9Ppk9oU92VO8IUHnc6oP5ov7f9PQ1NLIO5EHcqghOJvRoV7taA/vCd26wDD3pMSPKP490GjCvjlkmOC5pRJ47qJo+Xx/mi743t',
         'p2p': sirius_sdk.P2PConnection(
-            my_keys=('6AxpcFTPihyWtjWQ35ehnnpJqcP1mmkFecGPgApo28G5',
-                     '33WzfeKMVjcCpNSJEUoXbdSZo174xk1U2aG9ZAwTsWsomw2Dr7MJTkXRxU6CPStr2PJoTdpRtseFCkKo29aDD4BX'),
-            their_verkey='LQuue52xRKEYcZmQ84fzz2dGyXuv5LbyM12pejUg9Hy'
+            my_keys=('5c7VPHiRXpEarAS3bwExURRQpNet2YqTboBjfT77GSUt', '23YcJp98mdKqM86Zh9q8hQN2JqrjaonfXGNJeuvtzsbyunKo4KosNpQFScYoceGhTjymxSwjDnxMmBGho834drTv'),
+            their_verkey='5YtTtPa6Q1x2Xy7EMhJts3xqVNZtudjRDoqZD2Mhg8yN'
         )
-    }
+    },
+    # Does not make sense for Holder
+    'DID': 'UXE2pJUCC3mDgU5UBKyLXX',
+    'VERKEY': 'FzzouvZ4MqENPXxGwue3S1zguucpD47FbedpUxb31qwS'
 }
+
+
+LAB = {
+    'SDK': {
+        'server_uri': 'https://demo.socialsirius.com',
+        'credentials': b'hMPfx0D1ptQa2fK8UPw7p9/Zf/UUEY9Ppk9oU92VO8IUHnc6oP5ov7f9PQ1NLIO5EHcqghOJvRoV7taA/vCd28f9cZJym6VMb+wus3RwPeyV1ze+MeCZhLZ+KRJzmdZR',
+        'p2p': sirius_sdk.P2PConnection(
+            my_keys=('5rzyEdyTkLyeJmMC9VUvEPx4hozkHkB47cXHwmS4T3Cu', '29Yj3tp2ewETQknzt1eZFkt1vn9riA8Abos1VA7E3KAgx1UbsBVknTVvSKrioQsnoJEUV5heqLNuqghHnYcjQuUM'),
+            their_verkey='5MwrFLPP91phaEe5QjDAUhj4kmqQHkZszbNksFxqXWSM'
+        )
+    },
+    'DID': '9UVPoNm8fqhWUsnm6mcuGd',
+    'VERKEY': '5csxHQdoAyhi5VUeLsv97dtGnueeRRpHpE6XRuF8xGVP'
+}
+
+
+GOV = {
+    'SDK': {
+        'server_uri': 'https://demo.socialsirius.com',
+        'credentials': b'hMPfx0D1ptQa2fK8UPw7p9/Zf/UUEY9Ppk9oU92VO8IUHnc6oP5ov7f9PQ1NLIO5EHcqghOJvRoV7taA/vCd29jfRMZ9/VdiFHKHxS3FdtMT19uY7Os7ERst14ASRcf5',
+        'p2p': sirius_sdk.P2PConnection(
+            my_keys=('3r8TRFsAZZNjmxFb8Hb5XVuJm7rdttyVcqbhLb6z1ay9', '2z4ED7MskwBrrSgzdcHSzusrqhDajUidrrGXXHrNvzfGoxvDDbM6pXUzKXVB6TSdoaVQUN8sgBn7UbrnfA15dKVD'),
+            their_verkey='3x82GoLgC3WZ2nnjYZaqwFnbtaq9kxXEMRf2QHq1bF5G'
+        )
+    },
+    'DID': 'UZ6ULjvZj4Pog7SDrKxXGx',
+    'VERKEY': 'G21uvChANhXCxXVe7F9VQeAc6eyt7fZ1hYHHRR3YVNry'
+}
+
 
 GOV_AGENT_CREDS = VERIFIER
 GOV_DID = GOV_AGENT_CREDS['DID']
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import asyncio
 
 import sirius_sdk
-from defs import ISSUER, VERIFIER, SCHEMA_IDS
+from defs import ISSUER, VERIFIER, HOLDER, LAB, GOV, SCHEMA_IDS
 
 
 async def check_health():
@@ -26,6 +26,37 @@ async def check_health():
         for schema_id in SCHEMA_IDS:
             print(f'\n>Schema: {schema_id}')
             schema = await dkms.load_schema(schema_id, ISSUER['DID'])
+            print(json.dumps(schema.body, indent=2, sort_keys=True))
+    print('========= HOLDER ==========')
+    async with sirius_sdk.context(**HOLDER['SDK']):
+        meta = await sirius_sdk.DID.get_my_did_with_meta(HOLDER['DID'])
+        assert meta['verkey'] == HOLDER['VERKEY']
+        dkms = await sirius_sdk.ledger('indicio_test_network')
+        # See all: https://github.com/thecardeaproject/cardea/tree/main/schemas
+        for schema_id in SCHEMA_IDS:
+            print(f'\n>Schema: {schema_id}')
+            schema = await dkms.load_schema(schema_id, HOLDER['DID'])
+            print(json.dumps(schema.body, indent=2, sort_keys=True))
+    print('========= LAB ==========')
+    async with sirius_sdk.context(**LAB['SDK']):
+        meta = await sirius_sdk.DID.get_my_did_with_meta(LAB['DID'])
+        assert meta['verkey'] == LAB['VERKEY']
+        dkms = await sirius_sdk.ledger('indicio_test_network')
+        # See all: https://github.com/thecardeaproject/cardea/tree/main/schemas
+        for schema_id in SCHEMA_IDS:
+            print(f'\n>Schema: {schema_id}')
+            schema = await dkms.load_schema(schema_id, LAB['DID'])
+            print(json.dumps(schema.body, indent=2, sort_keys=True))
+    print('========= GOV ==========')
+    async with sirius_sdk.context(**GOV['SDK']):
+        dids = await sirius_sdk.DID.list_my_dids_with_meta()
+        meta = await sirius_sdk.DID.get_my_did_with_meta(GOV['DID'])
+        assert meta['verkey'] == GOV['VERKEY']
+        dkms = await sirius_sdk.ledger('indicio_test_network')
+        # See all: https://github.com/thecardeaproject/cardea/tree/main/schemas
+        for schema_id in SCHEMA_IDS:
+            print(f'\n>Schema: {schema_id}')
+            schema = await dkms.load_schema(schema_id, GOV['DID'])
             print(json.dumps(schema.body, indent=2, sort_keys=True))
 
 
